9a4c621a63e04d1ae13c0151ed02db6e5457fd84
Plamen Yankov <p.s.yankov@abv.bg>
2021-10-13 09:36:57 +0300

    cin and conditionals

--- OOP/week_1/main.cpp
+++ OOP/week_1/main.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello BSc CS" << std::endl;
-    return 0;
+    while (true)
+    {
+        std::cout << "1: Print Help" << std::endl;
+        std::cout << "2: Print exchange stats" << std::endl;
+        std::cout << "3: Make an offer" << std::endl;
+        std::cout << "4: Make a bid" << std::endl;
+        std::cout << "5: Print wallet" << std::endl;
+        std::cout << "6: Continue" << std::endl;
 
+        std::cout << "===============" << std::endl;
+        std::cout << "Type in 1-6" << std::endl;
+
+        int userOption;
+        std::cin >> userOption;
+        std::cout << "You chose: " << userOption << std::endl;
+
+        if (userOption == 0)
+        {
+            std::cout << "Invalid choice. Choose 1-6" << std::endl; 
+        }
+        if (userOption == 1)
+        {
+            std::cout << "Help - Your aim is to make money. Analyse the market and make bids and offers." << std::endl; 
+        }
+        if (userOption == 2)
+        {
+            std::cout << "Market looks good" << std::endl; 
+        }
+        if (userOption == 3)
+        {
+            std::cout << "Make an offer - enter the amount" << std::endl; 
+        }
+        if (userOption == 4)
+        {
+            std::cout << "Make a bid - enter the amount" << std::endl; 
+        }
+        if (userOption == 5)
+        {
+            std::cout << "Your wallet is empty" << std::endl; 
+        }
+        if (userOption == 6)
+        {
+            std::cout << "Going to next time frame" << std::endl; 
+        }
+    }
+    
+
+    return 0;
+    // How to run?
+    // g++ main.cpp -o merklerex
+    // ./merklerex
 }
