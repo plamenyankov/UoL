e2b4f03ee344f335119065dba4d26fa8e3ef05cf
Plamen Yankov <p.s.yankov@abv.bg>
2021-10-13 17:00:41 +0300

    add solar template

--- GP/week_1/emptyExample/sketch.js
+++ GP/week_1/emptyExample/sketch.js
@@ -1,15 +1,30 @@
 function setup() {
     createCanvas(900, 600);
     background(0);
+    // rectMode(CENTER);
 }
 
 function draw() {
     background(0);
-    fill(255);
-    rect(0, 0, 50, 50);
+    // fill(255);
+    // rect(0, 0, 50, 50);
+    push();
+    fill(255,0,0);
+    translate(200,200);
+    rotate(radians(map(second(),0,60,0,360)));
 
+    ellipse(0,0,30,30);
+    pop();
+    push();
     fill(125);
-    translate(200,100);
     rotate(radians(45));
-    rect(0,0,50,50);
+    translate(200,100);
+    rect(0,0,100,100);
+    pop();
+    push();
+    fill(255);
+    textSize(32);
+    text(map(second(),0,60,0,360), 300, 300);
+    pop();
+    
 }
