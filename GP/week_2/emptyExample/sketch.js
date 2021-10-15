c10e90036661c65758752903996dd8f55860adbe
Plamen Yankov <p.s.yankov@abv.bg>
2021-10-15 10:32:26 +0300

    bouncing ball

---
+++ GP/week_2/emptyExample/sketch.js
@@ -0,0 +1,37 @@
+
+var ball;
+function setup() {
+    createCanvas(900, 600);
+    ball = new Ball();
+}
+
+function draw() {
+    background(0);
+    ball.run();
+}
+
+class Ball{
+    constructor(){
+        this.speedX = random(-2, 2);
+        this.speedY = random(-2, 2);
+        this.locX = random(width);
+        this.locY = random(height);
+    }
+    run(){
+        this.draw();
+        this.move();
+        this.bounce();
+    }
+    draw(){
+        fill(125);
+        ellipse(this.locX, this.locY, 40, 40);
+    }
+    move(){
+        this.locX += this.speedX;
+        this.locY += this.speedY;
+    }
+    bounce(){
+        if(this.locX < 1 || this.locX > width) this.speedX *= -1;
+        if(this.locY < 1 || this.locY > height) this.speedY *= -1;
+    }
+}
