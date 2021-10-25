1c235ab8aac296f5790a3724050212d1a29d3bdb
Plamen Yankov <p.s.yankov@abv.bg>
2021-10-25 20:26:34 +0300

    body parser and form validation

--- DNW/backup/topic4/htmlExpress/index.js
+++ DNW/backup/topic4/htmlExpress/index.js
@@ -1,7 +1,9 @@
 // The index.js file of your application
 const express = require("express");
+const bodyParser= require ("body-parser");
 const app = express();
 const port = 8086;
+app.use(bodyParser.urlencoded({ extended: true }));
 require("./routes/main")(app);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
--- DNW/backup/topic4/htmlExpress/routes/main.js
+++ DNW/backup/topic4/htmlExpress/routes/main.js
@@ -12,4 +12,11 @@ module.exports = function (app) {
     //searching in the database
         res.send("This is the keyword you entered: " + req.query.keyword + "<br>" + "This is the result of the search: "); 
     });
+    app.get("/register", function (req,res) {
+        res.render("register.html");
+    });
+    app.post("/registered", function (req,res) {
+        // saving data in database
+     res.send(req.body)
+    });
 }
