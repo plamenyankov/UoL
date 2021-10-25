00eb250598b416ed91275b62b82213d013b0bcf1
Plamen Yankov <p.s.yankov@abv.bg>
2021-10-25 19:51:58 +0300

    all topics

---
+++ DNW/backup/topic2/mySecondNode/index.js
@@ -0,0 +1,11 @@
+// The index.js file of your application
+var http = require("http");
+var dt = require("./myFirstModule");
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("The date and time are currently: " + dt.myDateTime() + "\n");
+    res.end("Hello World again!");
+   
+}).listen(8082, function () {
+    console.log("Node server is running...");
+});
---
+++ DNW/backup/topic4/htmlExpress/routes/main.js
@@ -0,0 +1,15 @@
+// The main.js file of your application
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.render("index.html", {
+            title: "Dynamic title Bro"
+        });
+    });
+    app.get("/search", function (req, res) {
+        res.render("search.html");
+    });
+    app.get("/search-result", function (req, res) {
+    //searching in the database
+        res.send("This is the keyword you entered: " + req.query.keyword + "<br>" + "This is the result of the search: "); 
+    });
+}
